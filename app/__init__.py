aec9123a2c5855e0064d4c1f04a14e5c40bf945d
omkarmamid <omkarmamid3145@gmail.com>
2024-06-26 10:15:01 -0400

    I messed everything up, it was sooo good and now its soo bad :(

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,12 @@ import os
 def create_app():
     print("Creating Flask app...")
     app = Flask(__name__)
-    app.config['GOOGLE_MAPS_API_KEY'] = os.getenv('GOOGLE_MAPS_API')
-    app.config['AQ_MAPS_API_KEY'] = os.getenv('AQ_MAPS_API')
+    google_maps_api = os.getenv('GOOGLE_MAPS_API')
+    aq_maps_api = os.getenv('AQ_MAPS_API')
+    print(f"GOOGLE_MAPS_API: {google_maps_api}")
+    print(f"AQ_MAPS_API: {aq_maps_api}")
+    app.config['GOOGLE_MAPS_API_KEY'] = google_maps_api
+    app.config['AQ_MAPS_API_KEY'] = aq_maps_api
 
     # Import and register the blueprint from the routes module
     from .routes import main
