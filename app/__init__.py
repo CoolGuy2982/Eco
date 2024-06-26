6c14a7f6fcd113d4c5700ed684322062d6db9c68
omkarmamid <omkarmamid3145@gmail.com>
2024-06-26 08:59:42 -0400

    Logging :/

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 import os
 
 def create_app():
+    print("Creating Flask app...")
     app = Flask(__name__)
     app.config['GOOGLE_MAPS_API_KEY'] = os.getenv('GOOGLE_MAPS_API')
     app.config['AQ_MAPS_API_KEY'] = os.getenv('AQ_MAPS_API')
+
     # Import and register the blueprint from the routes module
     from .routes import main
     app.register_blueprint(main)
 
+    print("Flask app created successfully.")
     return app
