325af4b04c001bcf5d0cd1b6a7af8c3cd56ee4ba
omkarmamid <omkarmamid3145@gmail.com>
2024-06-26 08:30:52 -0400

    bruhhhh

--- apply.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-from flask_cors import CORS
-import os
-
-app = create_app()
-CORS(app)  # Enable CORS for all routes and origins
-
-if __name__ == "__main__":
-    apply.run(debug=True)
