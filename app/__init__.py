4492d9be398bb641b854649a5b3430c62e0ddc16
omkarmamid <omkarmamid3145@gmail.com>
2024-06-27 09:04:48 -0400

    Lets try this, got rid of useless env loading in init.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,9 @@ import os
 
 def create_app():
     app = Flask(__name__)
-    print("Ok we got this far...")
-    google_maps_api = os.getenv('GOOGLE_MAPS_API')
-    aq_maps_api = os.getenv('AQ_MAPS_API')
 
+    google_maps_api = os.getenv('GOOGLE_MAPS_API')
     app.config['GOOGLE_MAPS_API_KEY'] = google_maps_api
-    app.config['AQ_MAPS_API_KEY'] = aq_maps_api
 
     from routes import main
     app.register_blueprint(main)
