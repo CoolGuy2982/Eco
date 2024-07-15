937fc1402e5bb67835fca61e63d76ab96e80921a
omkarmamid <omkarmamid3145@gmail.com>
2024-07-15 16:19:48 -0400

    Update __init__.py

--- appp/__init__.py
+++ appp/__init__.py
@@ -3,7 +3,7 @@ import os
 
 def create_app():
     app = Flask(__name__)
-
+    app.secret_key = os.getenv('FLASK_SECRET_KEY', 'your_default_secret_key')
     google_maps_api = os.getenv('GOOGLE_MAPS_API')
     app.config['GOOGLE_MAPS_API_KEY'] = google_maps_api
 
